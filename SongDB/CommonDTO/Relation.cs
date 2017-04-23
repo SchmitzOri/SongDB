e01658a919703bb1795e40f04156be1420d90e73
Ori Schmitz <mr.schmitzori@gmail.com>
2017-04-23 16:24:51 +0300

    123asdasd123

---
+++ SongDB/CommonDTO/Relation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDTO
+{
+    [DataContract]
+    public class RelationAddRequest
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public Guid RelationType { get; set; }
+        [DataMember]
+        public Guid Word1 { get; set; }
+        [DataMember]
+        public Guid Word2 { get; set; }
+    }
+
+    [DataContract]
+    public class RelationAddResponse
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+    }
+
+
+    [DataContract]
+    public class RelationDeleteRequest
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+    }
+
+    [DataContract]
+    public class RelationDeleteResponse
+    {
+        [DataMember]
+        public bool Success { get; set; }
+    }
+}
